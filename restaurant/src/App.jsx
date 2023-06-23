54581610ff9f04c2fee13717a27bea553c9bd8d7
MidVC <lutony040720@gmail.com>
2023-06-23 20:41:22 +0800

    Added App.jsx and App.css to somehow (LOL?) make the text centered

---
+++ restaurant/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Reviews from "./pages/Reviews";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/reviews",
+    element: <Reviews />,
+  },
+]);
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
