76f35ab712b4a270edd8350563d8e62e50e448b5
Tommy Su <su.tommy23@gmail.com>
2023-06-23 11:37:36 -0400

    quick fix

--- restaurant/src/components/Footer.jsx
+++ restaurant/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='location'>
-        <p>123 We Accelerate Avenue</p>
+        <p>282 We Accelerate Avenue</p>
         <p>Waterloo, ON, Canada N2L 3G1</p>
       </div>
     </div>
