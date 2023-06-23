4bebd34c61054bfdc90344cee6584023cef70b31
MidVC <lutony040720@gmail.com>
2023-06-23 20:42:47 +0800

    Update main.jsx to be compatiable with the App

--- restaurant/src/main.jsx
+++ restaurant/src/main.jsx
@@ -1,40 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Menu from "./pages/Menu";
-import Contact from "./pages/Contact";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Reviews from "./pages/Reviews";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/reviews",
-    element: <Reviews />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
